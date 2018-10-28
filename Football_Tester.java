8ee97018745b1b94e4649bd9c039d03d7dbf11b8
NickitaTrofimov <32363673+NickitaTrofimov@users.noreply.github.com>
2018-10-28 20:07:51 +0300

    Update Football_Tester.java

--- Football_Tester.java
+++ Football_Tester.java
@@ -1,9 +1,6 @@
 package ru.mirea;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Scanner;
 
 public class Football_Tester extends JFrame {
     public static void main(String[]args)
